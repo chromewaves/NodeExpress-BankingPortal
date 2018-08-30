87c6665934964fb94fcd35d6b962678f8e7a1a83
Frank Yang <frank@chromewaves.net>
2018-08-30 16:09:51 -0400

    completed module

--- src/data.js
+++ src/data.js
@@ -1,19 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-)
-const accounts = JSON.parse(accountData)
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf8'
-)
-const users = JSON.parse(userData)
-
+const accounts = JSON.parse(fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8'))
+const users = JSON.parse(fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8'))
 const writeJSON = () => {
-  const accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), JSON.stringify(accounts, null, 4), 'utf8')
 }
 
 module.exports = { accounts, users, writeJSON }
