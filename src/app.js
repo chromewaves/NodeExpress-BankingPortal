898b697f9ff24acf659e113c217f3572122e63b1
Frank Yang <frank@chromewaves.net>
2018-08-30 15:46:49 -0400

    completed module

--- src/app.js
+++ src/app.js
@@ -53,9 +53,9 @@ app.get('/payment', (req, res) => {
 })
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount
-  accounts.credit.available -= parseInt(req.body.amount, 10)
+  accounts.credit.available += parseInt(req.body.amount, 10)
   const accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
-  res.render('payment', { message: 'Payment Successful', accounts: accounts.credit })
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit })
 })
 app.listen(3000, () => console.log('listening on 3000'))
