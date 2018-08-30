7d7513c3ef1ef293f00e5ab129e72a9b6f9f657b
Frank Yang <frank@chromewaves.net>
2018-08-30 15:42:55 -0400

    completed module

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 const accountData = fs.readFileSync(
   path.join(__dirname, 'json', 'accounts.json'), 'utf8'
@@ -19,6 +20,7 @@ const userData = fs.readFileSync(
   path.join(__dirname, 'json', 'users.json'), 'utf8'
 )
 const users = JSON.parse(userData)
+
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts })
@@ -33,7 +35,27 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.credit })
 })
 app.get('/profile', (req, res) => {
-  res.render('profile', { user: user[0] })
+  res.render('profile', { user: users[0] })
 })
-
+app.get('/transfer', (req, res) => {
+  res.render('transfer')
+})
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
+  accounts[req.body.to].balance = accounts[req.body.to].balance = req.body.amount
+  parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  res.render('transfer', { message: 'Transfer Completed' })
+})
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit })
+})
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount
+  accounts.credit.available -= parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  res.render('payment', { message: 'Payment Successful', accounts: accounts.credit })
+})
 app.listen(3000, () => console.log('listening on 3000'))
